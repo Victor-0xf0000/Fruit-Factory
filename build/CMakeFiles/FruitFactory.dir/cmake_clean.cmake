ddb900eaf1d13483f4ea7fe9ed9b0c76788f895d
Victor-0xf0000 <victor12315122008@gmail.com>
2021-05-08 18:39:15 -0300

    first commit

---
+++ build/CMakeFiles/FruitFactory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FruitFactory.dir/src/main.cpp.obj"
+  "FruitFactory.exe"
+  "FruitFactory.exe.manifest"
+  "FruitFactory.lib"
+  "FruitFactory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FruitFactory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
